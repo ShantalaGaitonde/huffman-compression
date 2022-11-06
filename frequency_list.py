6d1ccdca19138d70d242f42f6c1ae9d49a46d008
alinakons <117578611+alinakons@users.noreply.github.com>
2022-11-06 17:59:51 -0500

    Alina test changes
    Alina test changes

--- frequency_list.py
+++ frequency_list.py
@@ -4,7 +4,8 @@ letter_freq = {}
 
 
 def generate_frequency_dictionary(text):
-    for c in range(0, len(text)-1):
+    #minus one is deleted
+    for c in range(0, len(text)):
         if letter_freq.get(text[c]) is None:
             newnode = Node(text[c], 1)
             letter_freq.update({text[c]: newnode})
